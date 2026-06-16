2397bfc91edf4554d67c497909405d1ebb421227
nawaf152na-tech <nawaf152na@gmail.com>
2026-06-16 14:32:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,26 @@
-import asyncio
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = os.getenv("TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("أرسل رسالتك وسيتم تحويلها للدعم.")
+
+async def forward_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+
+    message = f"""
+📩 رسالة جديدة
+👤 {user.first_name}
+🆔 {user.id}
+
+💬 {update.message.text}
+"""
+
+    await context.bot.send_message(chat_id=ADMIN_ID, text=message)
+    await update.message.reply_text("تم إرسال رسالتك.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
@@ -7,11 +29,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_admin))
 
     print("Bot running...")
-
-    async def run():
-        await app.run_polling()
-
-    asyncio.run(run())
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
